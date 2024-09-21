894850ae8ada4ad3569b7b923a1287b0f7ac4ef5
Maxime Vernay <vernaymaxime4@gmail.com>
2024-09-21 11:00:57 +0200

    second step function

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ listStepForm = document.querySelectorAll(".formStep");
 listStepRound = document.querySelectorAll(".nav-round");
 btnUpStep = document.querySelectorAll(".btnUP");
 listFirstStepField = document.querySelectorAll(".input-step-one");
-let activeStep = 0
+listbtnDownStep = document.querySelectorAll(".btnDown");
+btnSwitch = document.querySelector(".yeat-month-switch-block");
+listPriceSelect = document.querySelectorAll(".price-text");
+listTextDiscount = document.querySelectorAll(".yearlyDiscount");
+switchButton = document.querySelector(".switch-button");
+listTextButtonSwitch = document.querySelectorAll(".text-switch");
+
+let activeStep = 1
+let tabPrice =[["$9/mo","$90/yr"],["$12/mo","$120/yr"],["$15/mo","$150/yr"]]
+let monthOrYear = 1;
 
 
 function f_changeActiveStep(change){
@@ -45,6 +54,30 @@ function f_check_for_error(step){
     }
 }
 
+function f_change_price(change){
+    for(let i = 0;i<listPriceSelect.length;i++){
+        listPriceSelect[i].innerText = tabPrice[i][change];
+        if (change){
+            listTextDiscount[i].innerText ="2 months free";
+            switchButton.style.justifyContent = "end";
+            listTextButtonSwitch[1].style.color = "var(--Marine-blue)"
+            listTextButtonSwitch[0].style.color = "var(--Cool-gray)"
+        }else{
+            listTextDiscount[i].innerText ="";
+            switchButton.style.justifyContent = "start";
+            listTextButtonSwitch[1].style.color = "var(--Cool-gray)"
+            listTextButtonSwitch[0].style.color = "var(--Marine-blue)"
+        }
+    }
+}
+function f_switchYearMonth(){
+    monthOrYear = monthOrYear*-1;
+    if (monthOrYear === -1 ){
+        f_change_price(1);
+    }else{
+        f_change_price(0)
+    }
+}
 
 f_changeActiveStep(0);
 
@@ -56,4 +89,13 @@ for (let i = 0;i<btnUpStep.length;i++ ){
      
     })
 }
+for (let i =0;i<listbtnDownStep.length;i++){
+    listbtnDownStep[i].addEventListener("click",function(){
+        console.log("down");
+        f_changeActiveStep(-1);
+    })
+}
 
+btnSwitch.addEventListener("click", function(){
+    f_switchYearMonth();
+})
